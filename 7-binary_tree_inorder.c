145c749ba9dfdd784cc42bb31b432bcf87d3d94f
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-08-30 11:30:11 +0300

    Fix betty and finish task 9 Fix betty for task 8 and complete task 9 to use post order traverse.

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_inorder - Goes through a binary tree using in-order traversal.
+ * @tree: pointer to the root of the tree.
+ * @func: A pointer to the function to call over the current node.
+ */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Goes through a binary tree using in-order traversal.
+ * @tree: pointer to the root of the tree.
+ * @func: A pointer to the function to call over the current node.
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
