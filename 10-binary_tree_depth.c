4b4c56e01c1df66a9ea0fffd88fc9f4e4ea0f858
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-08-31 10:43:07 +0300

    fix betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,8 +8,8 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if(tree != NULL && tree->parent != NULL)
+	if (tree != NULL && tree->parent != NULL)
 		return (binary_tree_depth(tree->parent) + 1);
-	
+
 	return (0);
 }
